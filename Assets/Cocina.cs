0e3b00cac5d327adab407b65afffbdf748712e31
Alumno08 e203 <a08e203@campustls.local>
2018-10-13 12:08:18 -0500

    Subiendo la tarea

---
+++ Assets/Cocina.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cocina : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Empezando a cocinar");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+      
+		
+	}
+
+    void Terminar() {
+        Debug.Log("Su comida esta lsita");
+    }
+}
---
+++ Assets/Contenedores.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contenedores : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void ColocandoIngredientes() {
+        Debug.Log("Los ingredientes estan en ");
+    }
+}
